f5462bfc7384d006bb40f06784d0abdfe02eb5ac
jverce <juanjov@gmail.com>
2019-06-17 22:04:38 -0700

    Customize responses

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ class MyBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said '${ context.activity.text }'`);
+            await context.sendActivity(`${ context.activity.text }`);
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
@@ -17,7 +17,7 @@ class MyBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome!');
+                    await context.sendActivity('hey... 😏');
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
