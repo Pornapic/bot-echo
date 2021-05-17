e8c13efec40946b692cf281fb32346ef69836ac1
jverce <juanjov@gmail.com>
2021-05-16 21:54:14 -0700

    Basic build and tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,19 @@ import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
-  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: [
+    "**/__tests__/*.test.ts",
+  ],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
 export default config;
